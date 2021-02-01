458f29c6fbbcc3b6aeefaf7d9d37c74c7e999093
alfredjordian <72225766+alfredjordian@users.noreply.github.com>
2021-01-31 23:46:58 -0500

    Add files via upload

---
+++ build/precache-manifest.ece83c3c64a580f35835fd236fb4cfe8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c45f01ae2e655eb0974ea77e779012",
+    "url": "/index.html"
+  },
+  {
+    "revision": "289d2c07d513d629f353",
+    "url": "/static/js/2.4bf62a69.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.4bf62a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6adbae58e563ea04624",
+    "url": "/static/js/main.3e3e0f52.chunk.js"
+  },
+  {
+    "revision": "1316b828eda579fe06fd",
+    "url": "/static/js/runtime-main.86007643.js"
+  }
+]);
